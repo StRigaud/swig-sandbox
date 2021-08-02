920e538f4da0e6e582a73b8493924eabb9d5a85b
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-08-02 13:18:44 +0200

    improve comment

--- python_clesperanto_test.py
+++ python_clesperanto_test.py
@@ -2,30 +2,31 @@
 import build.swig.python.pyclesperanto as pycle
 import numpy as np
 
-# pure python numpy
+# python: numpy array generation, must be float32 for c++ float compatibility
 arr1d = np.array([1,2,3,4,5,6,7,8,9], dtype=np.float32)
 arr2d = np.array([[1,2,3],[4,5,6]], dtype=np.float32)
 arr3d = np.array([[[1,2,3],[4,5,6],[7,8,9]],[[10,11,12],[13,14,15],[16,17,18]]], dtype=np.float32)
 
-# C++ side: init
+# C++: initialisation of GPU
+cle = pycle.cle()
 
-gpu = pycle.cle()
-
-# C++ side: push
+# C++: push numpy array in buffer
 
 print("C++ push array 1d:")
-buffer_1d = gpu.push(arr1d)
+buffer_1d = cle.push(arr1d)
 buffer_1d.GetInfo()
 
 print("C++ push array 2d:")
-buffer_2d = gpu.push(arr2d)
+buffer_2d = cle.push(arr2d)
 buffer_2d.GetInfo()
 
 print("C++ push array 3d:")
-buffer_3d = gpu.push(arr3d)
+buffer_3d = cle.push(arr3d)
 buffer_3d.GetInfo()
 
+# C++: create buffer from size/type input
+
 print("C++ create array 3d:")
-new_buffer_3d = gpu.create(np.array([3,3,3], dtype=np.float32))
+new_buffer_3d = cle.create(np.array([3,3,3], dtype=np.float32))
 new_buffer_3d.GetInfo()
 
