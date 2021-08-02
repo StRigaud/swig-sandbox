a061b53e91ecf89cc006ae6d6ba7a9646d4096d2
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-08-02 12:11:24 +0200

    add python test and setup

---
+++ python_clesperanto_test.py
@@ -0,0 +1,31 @@
+
+import build.swig.python.pyclesperanto as pycle
+import numpy as np
+
+# pure python numpy
+arr1d = np.array([1,2,3,4,5,6,7,8,9], dtype=np.float32)
+arr2d = np.array([[1,2,3],[4,5,6],[7,8,9]], dtype=np.float32)
+arr3d = np.array([[[1,2,3],[4,5,6],[7,8,9]],[[10,11,12],[13,14,15],[16,17,18]]], dtype=np.float32)
+
+# C++ side: init
+
+gpu = pycle.cle()
+
+# C++ side: push
+
+print("C++ push array 1d:")
+buffer_1d = gpu.push(arr1d)
+buffer_1d.GetInfo()
+
+print("C++ push array 2d:")
+buffer_2d = gpu.push(arr2d)
+buffer_2d.GetInfo()
+
+print("C++ push array 3d:")
+buffer_3d = gpu.push(arr3d)
+buffer_3d.GetInfo()
+
+print("C++ create array 3d:")
+new_buffer_3d = gpu.create(np.array([3,3,3], dtype=np.float32))
+new_buffer_3d.GetInfo()
+
