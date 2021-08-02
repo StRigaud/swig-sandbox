f998fdaf29426761dc669f16049b69f1bba1ac77
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-08-02 13:13:56 +0200

    change array dimension to identify z,y,x dim order

--- python_clesperanto_test.py
+++ python_clesperanto_test.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # pure python numpy
 arr1d = np.array([1,2,3,4,5,6,7,8,9], dtype=np.float32)
-arr2d = np.array([[1,2,3],[4,5,6],[7,8,9]], dtype=np.float32)
+arr2d = np.array([[1,2,3],[4,5,6]], dtype=np.float32)
 arr3d = np.array([[[1,2,3],[4,5,6],[7,8,9]],[[10,11,12],[13,14,15],[16,17,18]]], dtype=np.float32)
 
 # C++ side: init
