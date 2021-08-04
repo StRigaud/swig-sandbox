e7cf5bfd6d2525777232660c590f0217a934fe3a
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-08-04 11:01:12 +0200

    add light object class

---
+++ cleLightObject.h
@@ -0,0 +1,42 @@
+#ifndef __cleLightObject_h
+#define __cleLightObject_h
+
+#include "clic.h"
+
+#include <string>
+#include <fstream>
+
+namespace clic
+{
+    
+class LightObject
+{   
+
+
+public:
+    enum ObjectType {cleBuffer, cleFloat, cleInt}; 
+    enum DataType {Float, Char, UChar, Int, UInt, Short, UShort};
+
+    LightObject() = default;
+    ~LightObject() = default;
+
+    std::string GetObjectType() const;
+    std::string GetDataType() const;
+
+    bool IsObjectType(ObjectType) const;
+    bool IsDataType(DataType) const;
+
+
+protected:    
+    std::string ObjectTypeToString(const ObjectType) const;
+    std::string DataTypeToString(const DataType) const;
+    DataType StringToDataType(const std::string) const;
+
+private:
+    ObjectType O;
+    DataType T;
+};
+
+} // namespace clic
+
+#endif // __cleLightObject_h
