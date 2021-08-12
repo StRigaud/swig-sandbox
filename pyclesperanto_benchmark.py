e2c4af31b618a53f3e35ae497f530e0df6d5b456
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-08-12 16:49:02 +0200

    detail timing

--- pyclesperanto_benchmark.py
+++ pyclesperanto_benchmark.py
@@ -4,44 +4,53 @@ import time
 
 start_timestart_time = time.time()
 
-image = np.ones((512,512,100), dtype=np.float32)
-valid = np.ones((512,512,100), dtype=np.float32) * 101
+image = np.zeros((512,512,100), dtype=np.float32)
+valid = np.ones((512,512,100), dtype=np.float32) * 500
 
 start_time = time.time()
 
-
-# C++: initialisation of GPU
+exetime = time.time()
 cle.select_device()
+exetime = (time.time() - exetime)
+print("\tinit    : %s seconds ---" % (exetime))
 
+exetime = time.time()
 input = cle.push(image)
+exetime = (time.time() - exetime)
+print("\tpush    : %s seconds ---" % (exetime))
+exetime = time.time()
 output = cle.create(image.shape)
+exetime = (time.time() - exetime)
+print("\tcreate  : %s seconds ---" % (exetime))
 
 exetime = time.time()
 cle.add_image_and_scalar(input, output, 100)
 exetime = (time.time() - exetime)
-print("--- %s seconds ---" % (exetime))
+print("\tkernel 0: %s seconds ---" % (exetime))
 
 exetime = time.time()
 cle.add_image_and_scalar(output, input, 100)
 exetime = (time.time() - exetime)
-print("--- %s seconds ---" % (exetime))
+print("\tkernel 1: %s seconds ---" % (exetime))
 
 exetime = time.time()
 cle.add_image_and_scalar(input, output, 100)
 exetime = (time.time() - exetime)
-print("--- %s seconds ---" % (exetime))
+print("\tkernel 2: %s seconds ---" % (exetime))
 
 exetime = time.time()
 cle.add_image_and_scalar(output, input, 100)
 exetime = (time.time() - exetime)
-print("--- %s seconds ---" % (exetime))
+print("\tkernel 3: %s seconds ---" % (exetime))
 
 exetime = time.time()
 cle.add_image_and_scalar(input, output, 100)
 exetime = (time.time() - exetime)
-print("--- %s seconds ---" % (exetime))
+print("\tkernel 4: %s seconds ---" % (exetime))
 
+exetime = time.time()
 result = cle.pull(output)
-print("--- %s seconds ---" % (time.time() - start_time))
+print("\tpull    : %s seconds ---" % (time.time() - start_time))
 
+print("diff output vs valid = ", np.sum((result-valid).flatten()))
 print("final --- %s seconds ---" % (time.time() - start_timestart_time))
